d5e4126baf787a59935e3b682ce90a016965db1a
btekgit <boraytek@gmail.com>
2018-03-05 14:48:55 +0300

    simpler layer and new initialization works better than before

---
+++ gausslayer_simple.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 19:07:34 2018
+
+@author: btek
+"""
+from keras import backend as K
+from keras.engine.topology import Layer
+from keras.utils import conv_utils
+from keras import activations, regularizers, constraints
+from keras import initializers
+from keras.engine import InputSpec
+import numpy as np
+import tensorflow as tf
+
+def idx_init(shape, dtype='float32'):
+    idxs = np.zeros((shape[0], shape[1]))
+    c = 0
+    # assumes square filters
+    wid = np.int(np.sqrt(shape[0]))
+    hei =np.int(np.sqrt(shape[0]))
+    for x in np.arange(wid):  # / (self.incoming_width * 1.0):
+        for y in np.arange(hei):  # / (self.incoming_height * 1.0):
+            idxs[c, :] = np.array([x, y])
+            c += 1
+
+    return idxs
+
+def cov_init(shape, dtype='float32'):
+    
+    cov = np.identity(shape[1], dtype)
+    # shape [0] must have self.incoming_channels * self.num_filters
+    cov = np.repeat(cov[np.newaxis], shape[0], axis=0)
+    
+    #for t in range(shape[0]):
+    #    cov[t] = cov[t]
+    return cov
+
+def scale_init(shape, dtype='float32'):
+    sc = np.linspace(0.5, 1.6, shape[0])
+    sc=np.expand_dims(sc, axis=1)
+    sc=np.expand_dims(sc, axis=2)
+    print(sc)
+    return sc
+
+class GaussScaler(Layer):
+    def __init__(self, rank, filters,
+                 kernel_size,
+                 strides=1,
+                 padding='valid',
+                 data_format=None,
+                 dilation_rate=1,
+                 activation=None,
+                 use_bias=False,
+                 kernel_regularizer=None,
+                 **kwargs):
+        super(GaussScaler, self).__init__(**kwargs)
+        #def __init__(self, num_filters, kernel_size, incoming_channels=1, **kwargs):
+        self.rank = rank
+        self.filters = filters
+        self.kernel_size = conv_utils.normalize_tuple(kernel_size, rank, 'kernel_size')
+        self.strides = conv_utils.normalize_tuple(strides, rank, 'strides')
+        self.padding = conv_utils.normalize_padding(padding)
+        self.data_format = conv_utils.normalize_data_format(data_format)
+        self.dilation_rate = conv_utils.normalize_tuple(dilation_rate, rank, 'dilation_rate')
+        self.activation = activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.input_spec = InputSpec(ndim=self.rank + 2)
+                 
+        #self.input_shape = input_shape
+        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+            kwargs['input_shape'] = (kwargs.pop('input_dim'))
+        print(kwargs)
+        self.kernel_size = kernel_size
+        
+        self.num_filters = filters
+        #self.incoming_channels = incoming_channels
+       
+        super(GaussScaler, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        if self.data_format == 'channels_first':
+            channel_axis = 1
+        else:
+            channel_axis = -1
+        if input_shape[channel_axis] is None:
+            raise ValueError('The channel dimension of the inputs '
+                             'should be defined. Found `None`.')
+        input_dim = input_shape[channel_axis]
+        
+        self.input_channels = input_dim
+        kernel_shape = self.kernel_size + (input_dim, self.filters)
+        print(kernel_shape)
+
+        self.bias = None
+        # Set input spec.
+        self.input_spec = InputSpec(ndim=self.rank + 2,
+                                    axes={channel_axis: input_dim})
+        self.built = True
+        # Create a trainable weight variable for this layer.
+        
+        kernel_size = self.kernel_size
+        # Idxs Init
+        
+        
+        
+        
+        mu = np.array([kernel_size[0] // 2, kernel_size[1] // 2])
+
+
+        # Convert Types
+        self.mu = mu.astype(dtype='float32')
+
+        # Shared Parameters
+        # below works for only two dimensional cov 
+        self.cov = self.add_weight(shape=[input_dim*self.filters,2,2], 
+                                  name="cov", initializer=cov_init, trainable=False)
+        
+        
+        self.cov_scaler = self.add_weight(shape=(input_dim*self.filters,),
+                                          name='scaler',initializer=scale_init,
+                                          trainable=True)
+                                  #constraint=constraints.non_neg())
+        
+        print(self.cov)
+        print(self.cov_scaler)
+        
+        # below prepares a meshgrid. 
+        self.idxs = self.add_weight(shape=[kernel_size[0]*kernel_size[1],2], 
+                                   name="idxs", initializer=idx_init, trainable=False)
+        
+        super(GaussScaler, self).build(input_shape)  # Be sure to call this somewhere!
+        
+    
+    def U(self):
+  
+        e1 = (self.idxs - self.mu)
+   
+        #print(self.cov.shape)
+        #print(len(tf.unstack(self.cov,axis=0)))
+        #print( tf.linalg.inv(tf.unstack(self.cov,axis=0)[0]))
+        # tensorflow does not need scan it does the same op to all covs.
+        #cov_inv = self.cov
+        cov_scaled =self.cov_scaler*self.cov
+#        cov_scaled = tf.scalar_mul(self.cov_scaler,self.cov)
+#        print(self.cov.shape, self.cov_scaler.shape )
+#        cov_scaled = K.batch_dot(self.cov_scaler,self.cov, axes=[1,2])
+        cov_inv = tf.linalg.inv(cov_scaled)
+        print("cov_scaled :",cov_scaled.shape)
+        #cov_inv = K.map_fn(lambda x: tf.linalg.inv(x), elems=tf.unstack(self.cov,axis=0))
+       
+
+        e2 = K.dot(e1, K.transpose(cov_inv))
+        ex = K.batch_dot(e2, e1, axes=[[1], [1]])
+        result = K.exp(-(1 / 2.0) * ex)
+
+   
+
+
+        # Transpose is super important.
+        masks = K.reshape(result,(self.kernel_size[0],
+                                  self.kernel_size[1],self.input_channels,self.filters))
+        # sum normalization each filter has sum 1
+        sums = K.sum(masks, axis=(0, 1), keepdims=True)
+        #ums = sums * sums
+        #print("sums shape: ", sums.shape)
+        #print(sums)
+        # Sum normalisation
+        gain = K.constant(np.sqrt(2.0),dtype='float32')
+        masks = masks * (gain/K.sqrt(sums))
+        print("masks shape", masks.shape)
+        #print("masks mask", K.mean(masks))
+        return masks
+
+    def call(self, inputs):
+        
+        print(inputs.shape)
+        filters = self.U()
+        print(filters.shape)
+        #filters /= T.sum(filters, axis=(2, 3)).dimshuffle(0, 1, 'x', 'x')
+        # channel_first means tensofrlow
+        conved = K.conv2d(inputs, filters, padding=self.padding, 
+                               data_format=self.data_format)
+        return conved
+
+    def compute_output_shape(self, input_shape):
+        if self.data_format == 'channels_last':
+            space = input_shape[1:-1]
+            new_space = []
+            for i in range(len(space)):
+                new_dim = conv_utils.conv_output_length(
+                    space[i],
+                    self.kernel_size[i],
+                    padding=self.padding,
+                    stride=self.strides[i],
+                    dilation=self.dilation_rate[i])
+                new_space.append(new_dim)
+            return (input_shape[0],) + tuple(new_space) + (self.filters,)
+        if self.data_format == 'channels_first':
+            space = input_shape[2:]
+            new_space = []
+            for i in range(len(space)):
+                new_dim = conv_utils.conv_output_length(
+                    space[i],
+                    self.kernel_size[i],
+                    padding=self.padding,
+                    stride=self.strides[i],
+                    dilation=self.dilation_rate[i])
+                new_space.append(new_dim)
+            return (input_shape[0], self.filters) + tuple(new_space)
+
+
+#test
+def test():
+    import tensorflow as tf
+    from gausslayer import GaussScaler
+    import numpy as np
+    
+    tf.reset_default_graph()
+
+    from keras.losses import mse
+    import keras
+    from keras.datasets import mnist,fashion_mnist, cifar10
+    from keras.models import Sequential, Model
+    from keras.layers import Input, Dense, Dropout, Flatten
+    
+    
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    inputimg = x_train[0]
+    
+    y = y_train[0]
+    sh = (inputimg.shape[0],inputimg.shape[1],1)
+    inputs = Input(shape=sh, name='inputlayer')
+    node_gauss = GaussScaler(rank=2,filters=12,kernel_size=(7,7), 
+                             padding='same',name='gausslayer')(inputs)
+
+    model = Model(inputs=inputs, outputs=node_gauss)
+    sgd = keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss=mse, optimizer=sgd, metrics=['accuracy'])
+    model.summary()
+    inputimg2 = np.expand_dims(np.expand_dims(inputimg,axis=0), axis=3)
+    gauss_layer = model.get_layer('gausslayer')    
+    gauss_layer_var = gauss_layer.get_weights()
+    out = gauss_layer.get_output_at(0)
+    
+    scores = model.predict(inputimg2,  verbose=1)
+    print( model.get_layer('gausslayer').output )
+
+#with tf.Session() as sess:
+#    outval = sess.run(out, feed_dict={inputs:inputimg2})
+
+
+
---
+++ mnist_keras_gauss_input.py
@@ -0,0 +1,324 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''Trains a simple convnet on the MNIST dataset.
+
+Gets to 99.25% test accuracy after 12 epochs
+(there is still a lot of margin for parameter tuning).
+16 seconds per epoch on a GRID K520 GPU.
+'''
+from __future__ import print_function
+
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+import keras
+from keras.datasets import mnist,fashion_mnist, cifar10
+from keras.models import Sequential, Model
+from keras.layers import Input, Dense, Dropout, Flatten,AveragePooling2D, Maximum, Subtract, UpSampling2D
+from keras.layers import Conv2D, MaxPooling2D, MaxPooling3D,SeparableConv2D, Lambda, Concatenate
+from keras.regularizers import l2
+from keras import backend as K
+import tensorflow as tf
+from gausslayer_simple import GaussScaler
+import numpy as np
+from matplotlib import pyplot as plt
+from tensorflow.python import debug as tf_debug
+tf.reset_default_graph()
+sess = K.get_session()
+#sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+K.set_session(sess)
+
+
+
+def load_dataset_mnist_cluttered():
+    root = "../AdaptiveCNNBTEK/src/"
+    data = np.load(root+"mnist_cluttered_60x60_6distortions.npz")
+    
+
+    X_train, y_train = data['x_train'], np.argmax(data['y_train'], axis=-1)
+    X_valid, y_valid = data['x_valid'], np.argmax(data['y_valid'], axis=-1)
+    X_test, y_test = data['x_test'], np.argmax(data['y_test'], axis=-1)
+
+    # reshape for convolutions
+    DIM = 60
+    X_train = X_train.reshape((X_train.shape[0],  DIM, DIM))
+    X_valid = X_valid.reshape((X_valid.shape[0], DIM, DIM))
+    X_test = X_test.reshape((X_test.shape[0], DIM, DIM))
+
+    return X_train, y_train.astype('int32'), X_valid, y_valid.astype('int32'), X_test, y_test.astype('int32')
+
+batch_size = 128
+num_classes = 10
+epochs = 10
+
+from datetime import datetime
+now = datetime.now()
+timestr = now.strftime("%Y%m%d-%H%M%S")
+logdir = "tf_logs_cluttered/.../" + timestr + "/"
+
+# the data, shuffled and split between train and test sets
+#(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    
+#(x_train, y_train),(x_test, y_test) = fashion_mnist.load_data()
+# =============================================================================
+# x_train = x_train.astype('float32')
+# x_test = x_test.astype('float32')
+# x_train /= 255
+# x_test /= 255
+# =============================================================================
+(x_train, y_train, x_valid, y_valid, x_test, y_test) = load_dataset_mnist_cluttered()
+#(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+use_subset=False
+if use_subset:
+    x_train=x_train[1:2000,]
+    y_train=y_train[1:2000,]
+# input image dimensions
+img_rows, img_cols = x_train.shape[1], x_train.shape[2]
+if len(x_train.shape)>3:
+    nchannels = x_train.shape[3]
+else: 
+    nchannels = 1
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], nchannels, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], nchannels, img_rows, img_cols)
+    input_shape = (nchannels, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, nchannels)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, nchannels)
+    input_shape = (img_rows, img_cols, nchannels)
+
+
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+
+
+node_in  = Input(shape=input_shape)
+#node_s = Dropout(0.1)(node_in)
+enable_gauss = True
+calculate_dog = False
+up_down_sample = True
+if enable_gauss:
+    gauss_filters = 8
+    node_gauss = GaussScaler(rank=2,filters=gauss_filters,kernel_size=(7,7), 
+                          input_shape=input_shape, 
+                          padding='same',name='gausslayer')(node_in)
+    
+
+    
+    gaussian_channels =[]
+    conv_channels =  []
+    diff_channels =  []
+    for i in range(gauss_filters):
+        # separate channels
+        # use input directly at least in one channel
+        if i>0:
+            g_c= Lambda(lambda x: K.expand_dims(x[:,:,:,i],axis=3), name='ChannelSeparator'+str(i))(node_gauss)
+        else:
+            g_c = node_in
+        #g_c= Lambda(lambda x: K.expand_dims(x[:,:,:,i],axis=3), name='ChannelSeparator'+str(i))(node_gauss)
+                
+        
+        
+        
+        # downsample Gaussian layers, however leave the first one as it is. 
+        # we want a clear input as well
+        # this may be controlled by the cov_scaler value.
+        if up_down_sample and i>0:
+            g_c = AveragePooling2D(pool_size=(2,2))(g_c)
+            
+            
+        
+        g_c_shape = input_shape
+        gaussian_channels.append(g_c)
+        
+        # conv with filter
+        node_conv_gc = Conv2D(32, kernel_size=(3, 3), padding='same',
+                     activation='tanh', input_shape=input_shape)(g_c)
+        
+        if up_down_sample and i>0:
+            node_conv_gc = UpSampling2D(size=(2,2))(node_conv_gc)
+        
+        conv_channels.append(node_conv_gc)
+        
+        # this calculates scale-space with DoG
+        if i >0 and calculate_dog:
+            diff_layer = Subtract()([g_c,gaussian_channels[i-1]])
+            diff_channels.append(diff_layer)
+        
+    
+    # depthwise pooling layer
+    if not calculate_dog:
+        node_conv1 = Maximum(name='merge')(conv_channels)
+        # try finding two groups 
+# =============================================================================
+#         rand_ix = np.random.permutation(len(conv_channels))
+#         i1, i2 = rand_ix[0:int(len(conv_channels)/2)],rand_ix[int(len(conv_channels)/2):len(conv_channels) ]
+#         set1 = [conv_channels[i] for i in i1]
+#         set2 = [conv_channels[i] for i in i2]
+#         grp_Max1 = Maximum(name='grpMax1')(set1)
+#         grp_Max2 = Maximum(name='grpMax2')(set2)
+#         node_conv1 = Concatenate(name='merge')([grp_Max1,grp_Max2])
+# =============================================================================
+    else:
+        node_conv1 = Concatenate(name='merge',axis=-1)(diff_channels)
+    
+    
+    
+    
+    
+    #output_at_gauss = node_gauss.
+    #node_gauss= AveragePooling2D(pool_size=(2, 2))(node_gauss)
+    #node_conv1 =K.depthwise_conv2d(30, kernel_size=(3, 3),
+    #             activation='relu')
+    #node_conv1 =SeparableConv2D(30, kernel_size=(3, 3), depth_multiplier=1,
+    #             activation='relu')(node_gauss)#, input_shape=input_shape))
+
+else:
+    node_conv1 = Conv2D(16, kernel_size=(3, 3),
+                     activation='relu', padding='same',
+                     input_shape=input_shape)(node_in)
+
+    node_conv1 = Conv2D(16, kernel_size=(3, 3),
+                     activation='relu', padding='same',
+                     input_shape=input_shape)(node_conv1)
+
+node_pool1= MaxPooling2D(pool_size=(2, 2))(node_conv1)
+#node_pool1= AveragePooling2D(pool_size=(2, 2))(node_conv1)
+node_conv2 = Conv2D(64, (3, 3), activation='relu',
+                    kernel_regularizer=l2(0.00001))(node_pool1)
+node_mxpool2 = MaxPooling2D(pool_size=(2, 2))(node_conv2)
+node_drp1 =Dropout(0.25)(node_mxpool2)
+
+node_flt= Flatten()(node_drp1)
+node_dns1=Dense(128, activation='relu',kernel_regularizer=l2(0.0001))(node_flt)
+node_drp=Dropout(0.5)(node_dns1)
+pred=Dense(num_classes, activation='softmax')(node_drp)
+model = Model(inputs=node_in, outputs=[pred])
+model.summary()
+
+# this is supposed write the training data.
+tb_call_back = keras.callbacks.TensorBoard(log_dir=logdir, 
+                                         histogram_freq=0, 
+                                         write_graph=False,
+                                         write_grads=False,
+                                         write_images=False)
+keras.callbacks.CSVLogger('epoch_results'+timestr+'.csv', separator=',', append=False)
+if enable_gauss:
+    from keras_utils import WeightHistory as WeightHistory
+    wh= WeightHistory()
+    #U_func = K.function(inputs=[model.input], outputs=[model.get_layer('gausslayer').U()])
+    #cov_dump = keras.callbacks.LambdaCallback([model.get_layer('gausslayer').U()])
+#out_call_back = keras.callbacks.Callback()
+#checkpoint = keras.callbacks.ModelCheckpoint('./Model' + '/weights-{epoch:02d}.h5', monitor='accuracy',
+#                                       save_best_only=True, save_weights_only=False, verbose=1)
+
+#toptimizer = keras.optimizers.Nadam(lr=0.0002, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004)
+toptimizer = keras.optimizers.Adadelta(decay=0.0001)
+loss_funct = keras.losses.categorical_crossentropy
+#toptimizer = keras.optimizers.SGD(lr=0.05)
+model.compile(loss=loss_funct,
+              optimizer=toptimizer,
+              metrics=['accuracy'])
+
+if enable_gauss:
+    gauss_layer = model.get_layer('gausslayer')
+    U_func = K.function(inputs=[model.input], outputs=[gauss_layer.U()])
+    U_val=U_func([np.expand_dims(x_test[0], axis=0)])[0]
+    num_filters = U_val.shape[-1]
+    n_row = int(num_filters/4)
+    n_col = 4
+
+    plt.subplots(n_row,n_col,figsize=(10,10))
+    plt.title('Gauss Filters before training')
+    for ix in range(num_filters):
+        plt.subplot(n_row,n_col,ix+1)
+        plt.imshow(U_val[:,:,0,ix])
+        plt.colorbar()
+    plt.show(block=False)
+
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=150,
+          verbose=1,
+          validation_data=(x_test, y_test), callbacks=[tb_call_back,wh])
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+if enable_gauss:
+    gauss_layer = model.get_layer('gausslayer')    
+    gauss_layer_var = gauss_layer.get_weights()
+    depthwise_pool = model.get_layer('merge')
+    intermediate_layer_model = Model(inputs=model.input,
+                                     outputs=[gauss_layer.output,depthwise_pool.output])
+    U_func = K.function(inputs=[model.input], outputs=[gauss_layer.U()])
+    U_val=U_func([np.expand_dims(x_test[0], axis=0)])[0]
+    for i in range(0,gauss_filters):
+        print("Covs: \n", gauss_layer_var[0][i])
+        
+    intermediate_outputs = intermediate_layer_model.predict(np.expand_dims(x_test[0], axis=0))
+    num_filters = intermediate_outputs[0].shape[-1]
+    n_row = int(num_filters/4)
+    n_col = 4
+
+
+    plt.subplots(n_row,n_col,figsize=(10,10))
+    plt.title('Outputs')
+    for ix in range(num_filters):
+        plt.subplot(n_row,n_col,ix+1)
+        plt.imshow(intermediate_outputs[0][0,:,:,ix])
+    plt.show(block=False)
+
+    num_filters = intermediate_outputs[1].shape[-1]
+    n_row = int(num_filters/4)
+    n_col = 4
+
+
+    plt.subplots(n_row,n_col,figsize=(12,12))
+    plt.title('Pooled outputs')
+    
+    for ix in range(num_filters):
+        plt.subplot(n_row,n_col,ix+1)
+        plt.imshow(intermediate_outputs[1][0,:,:,ix])
+    plt.show(block=False)
+    plt.tight_layout()
+
+
+    U_func = K.function(inputs=[model.input], outputs=[gauss_layer.U()])
+    U_val=U_func([np.expand_dims(x_test[0], axis=0)])[0]
+    num_filters = U_val.shape[-1]
+    n_row = int(num_filters/4)
+    n_col = 4
+    plt.subplots(n_row,n_col,figsize=(10,10))
+    plt.title('Filters after')
+    for ix in range(num_filters):
+        plt.subplot(n_row,n_col,ix+1)
+        plt.imshow(U_val[:,:,0,ix])
+        plt.colorbar()
+    plt.show(block=False)
+
+
+    # plot cov changes
+    sc_list = wh.get_list()
+    cov_arrays = (np.asarray(sc_list)).squeeze()
+    n_row = int(num_filters/4)
+    n_col = 4
+    plt.subplots(n_row,n_col,figsize=(12,12))
+    plt.title('Cov changes after batches')
+    for ix in range(num_filters):
+        plt.subplot(n_row,n_col,ix+1)
+        plt.plot(cov_arrays[0:10000,ix])
+    plt.show(block=False)
+
+
